d71191d7ae14f600e36e8b4d9c4c4f78b27d74e3
baoware <137852550+baoware@users.noreply.github.com>
2025-02-21 17:13:09 -0500

    Edit admin page.:

--- users/admin.py
+++ users/admin.py
@@ -5,9 +5,19 @@ from .models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role', 'is_librarian', 'is_patron')
+    list_display = ('user', 'user_email', 'role', 'is_librarian', 'is_patron')
     list_filter = ('role',)
     search_fields = ('user__username',)
+
+
+    # function to display email
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'Email'  
+
+    # allow role to be edited
+    list_editable = ('role',)  
+
 
 admin.site.register(UserProfile, UserProfileAdmin)
 
