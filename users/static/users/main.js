7a2d4de2ce671a03607e2d421e673337adf4df74
aziza50 <aziza.ergasheva50@gmail.com>
2025-02-22 14:11:47 -0500

    Updated dashboard for librarian and patron

--- users/static/users/main.js
+++ users/static/users/main.js
@@ -27,12 +27,189 @@ function myMap() {
     ];
 
     var mapProp= {
-        center:new google.maps.LatLng(38.0329173,-78.506895),
-        zoom:17,
-        disableDefaultUI: true
-    };
+        center:new google.maps.LatLng(38.0341423,-78.5082731),
+        zoom:15,
+        disableDefaultUI: true,
+        styles:
+        [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+};
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    const fetchMarker = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconarchive.com%2Fshow%2Fflat-vector-map-marker-icons-by-icons-land%2FMarker-1-PushPin-Green-icon.html&psig=AOvVaw0kjx0T3hNxydszdRfD3_e2&ust=1740326186800000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjFn7PS14sDFQAAAAAdAAAAABAl'
+    const fetchMarker = "https://icons.iconarchive.com/icons/icons-land/flat-vector-map-marker/48/Marker-1-PushPin-Green-icon.png";
     /**
     *Loop through all markers and add it to the current map
     */
@@ -40,9 +217,18 @@ function myMap() {
         const marker = new google.maps.Marker({
         position: { lat: markers[i].lat, lng:markers[i].lng},
         map:map,
-        icon: fetchMarker.
+        icon: fetchMarker,
         title: markers[i].locationName
-    })
+        });
+
+        const infoWindow = new google.maps.InfoWindow({
+            content: `<strong>${markers[i].locationName}</strong><br>${markers[i].address}`
+        });
+
+        marker.addListener("click", () => {
+            infoWindow.open(map, marker);
+        });
+
     }
 
 
