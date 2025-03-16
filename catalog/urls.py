2f7bd2f286c6a46eee8b9e596e39927bc84f3a47
Edward Chen <edward9065@gmail.com>
2025-03-15 22:49:19 -0400

    make it possible to change book cover image

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,6 @@ app_name='catalog'
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('add-book/', views.add_book, name='add_book'),  
+    path('<int:book_id>/', views.book_detail, name='book_detail'),
+    path('<int:book_id>/edit/', views.edit_book, name='edit_book'),  
 ]
