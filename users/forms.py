674a636c6b4fe7fcf058cea3d9dc69781971f8cb
JacobPlummer02 <123589437+JacobPlummer02@users.noreply.github.com>
2025-04-13 02:16:41 -0400

    fix: add widget configuration for profile_pic in ProfilePictureForm

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import UserProfile
+from django.forms.widgets import FileInput
 
 class ProfilePictureForm(forms.ModelForm):
     class Meta:
@@ -7,6 +8,9 @@ class ProfilePictureForm(forms.ModelForm):
         fields = ['profile_pic']
         labels = {
             'profile_pic': 'Profile Picture',
+        }
+        widget = {
+            'profile_pic': FileInput(attrs={'class': 'custom-file-input'}),
         }
 
     def save(self, commit=True):
