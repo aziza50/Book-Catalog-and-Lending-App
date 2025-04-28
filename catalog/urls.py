ef6588d46d16d5e32691d4f18a1c58f43484bee9
JacobPlummer02 <123589437+JacobPlummer02@users.noreply.github.com>
2025-04-28 00:21:20 -0400

    Add manage patrons functionality with view and template
    - Implemented a new view to manage patrons, allowing promotion to librarian.
    - Created a new template for displaying and managing patrons.
    - Updated URLs to include the manage patrons path.
    - Added a link to manage patrons in the base template.

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.browse_all_books, name='book_list'),
     path('add-book/', views.lend_book, name='lend_book'),
+    path('manage_patrons/', views.manage_patrons, name='manage_patrons'),
     path('add-comment/<int:book_id>/', views.add_comment, name='add_comment'),
     path('item/<int:book_id>/', views.item, name='item'),
     path("edit_item/<int:book_id>/", views.edit, name="edit_book"),
